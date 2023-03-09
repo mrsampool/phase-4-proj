b891a645bc81e909479c5b01bfdd8937f67e1ddb
Maika Harrell <mikahado@gmail.com>
2023-03-09 01:25:54 -0500

    initial set up via noyes build_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  
-  # Routing logic: fallback requests for React Router.
+    # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
+  # full CRUD will be 'resources: commands' or whatever model needed
+
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

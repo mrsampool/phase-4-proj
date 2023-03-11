fa3ca1ecc513a728be947f4746f3922a9092f4a6
Maika Harrell <mikahado@gmail.com>
2023-03-11 14:17:46 -0500

    initial commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,7 +52,7 @@ RSpec.configure do |config|
   #     RSpec.describe UsersController, type: :controller do
   #       # ...
   #     end
-  #
+  # note
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!

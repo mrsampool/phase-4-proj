404320baed1bf07141b649e7530ad14a332f5aa1
Maika Harrell <mikahado@gmail.com>
2023-03-11 23:50:07 -0500

    updates punchcard controller

--- app/controllers/punchcards_controller.rb
+++ app/controllers/punchcards_controller.rb
@@ -1,2 +1,44 @@
 class PunchcardsController < ApplicationController
+    before_action :authorize
+
+    def index 
+        punchcard = Punchcard.all
+        render json: punchcard
+    end
+
+    def create 
+        punchcard = Punchcard.create(:punchcard_params)
+        render json: punchcard, status: :created
+    end
+
+    def show
+        punchcard = Punchcard.find_by(id: params[:id])
+        render json: punchcard
+    end
+
+    def destroy
+        punchcard = Punchcard.find_by(id: params[:id])
+        punchcard.destroy
+    end
+
+    def update
+        punchcard = Punchcard.find_by(id: params[:id])
+        punchcard.update(punchcard_params)
+        render json: punchcard, status: :created
+    end
+
+    private 
+
+    def current_user
+        user = User.find_by(id: session[:user_id])
+    end
+
+    def punchcard_params
+        params.permit(:name, :type, :count, :qr_code)
+    end
+
+    def authorize
+        return render json: {error: "Not authorized"}, status: :unauthorized, unless session.include? :user_id
+    end
+
 end

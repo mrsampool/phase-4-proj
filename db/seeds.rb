c674bc4ddc8df3849d2896e279b3293ae5162a9e
Maika Harrell <mikahado@gmail.com>
2023-03-19 18:05:16 -0400

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,3 @@ c1 = Customer.create(username: "Smooky Buggins")
 c2 = Customer.create(username: "Pooky Beggins")
 c3 = Customer.create(username: "Gooky Boggins")
 
-c1card = customer.punchcards.create(name: starbucks, kind: 'cafe', count: 10, reward: "1/2 Off", user_id: 1)
-c2card = customer.punchcards.create(name: starbucks, kind: 'cafe', count: 10, reward: "1/2 Off", user_id: 1)
-c3card = customer.punchcards.create(name: starbucks, kind: 'cafe', count: 10, reward: "1/2 Off", user_id: 1)

5556caa7e218d39222a3c3dd8bcdf14b1e000084
Maika Harrell <mikahado@gmail.com>
2023-03-20 17:12:32 -0400

    adds pico.css

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { UserContext } from "./context/user"
 import { useNavigate } from 'react-router-dom'
+import '@picocss/pico/css/pico.min.css'
 
 const Login = () => {
 
